496335eaa4f4f9c0f4250917b12e88396148355e
jardeylson jacinto <jardeylsong.m@gmail.com>
2023-12-19 15:42:43 -0300

    feat: create class CreateOrderController.ts

---
+++ pedidos/src/modules/create-order.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { CreateOrderUseCase } from "./create-order.usecase";
+
+export class CreateOrderController {
+  async handle(request: Request, response: Response) {
+    const useCase = new CreateOrderUseCase()
+    const order = await useCase.execute(request.body)
+    return response.json(order)
+  }
+}
